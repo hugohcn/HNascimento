eb09f531360bdfacd9d6323f97b42183dc6e048a
Hugo Nascimento <hugohcn@gmail.com>
2014-12-17 01:35:26 -0200

    Contact form, Portfolio Section and some animations/js functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+/* Author: Hugo Nascimento
+ * Date: 16/12/2014
+ * Description: Main JS Project File.
+ */
 
+//Document Ready
+$(function(){
+	smoothScroll(650);
+});
+
+
+//Smooth Scroll Down
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top -50
+	        }, duration);
+	    }
+	});
+}
